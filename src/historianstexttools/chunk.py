696df94d37ef791f9904466be90b380c0f507eb9
Christopher Goodwin <54195497+cgoodwinDH@users.noreply.github.com>
2024-05-18 14:33:07 -0500

    re-configured projected chunking techniques

--- src/historianstexttools/chunk.py
+++ src/historianstexttools/chunk.py
@@ -1,2 +1,12 @@
 def chunk(number_of_sentences=1, overlap=0, language='english'):
     pass
+
+def semantic_chunking():
+    pass
+
+def sentence_chunking():
+    pass
+
+def wordcount_chunking():
+    '''Chunks all documents by a specified and static word count. If document ends before chunk size is hit, it ends the chunk at end-of-document.'''
+    pass
