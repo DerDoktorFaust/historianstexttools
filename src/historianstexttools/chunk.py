855070e32e4c2e536751abb885882bbf80d16afd
Christopher Goodwin <54195497+cgoodwinDH@users.noreply.github.com>
2024-05-25 13:50:11 -0500

    created wordcount_chunking function

--- src/historianstexttools/chunk.py
+++ src/historianstexttools/chunk.py
@@ -1,12 +1,31 @@
 def chunk(number_of_sentences=1, overlap=0, language='english'):
     pass
 
-def semantic_chunking():
+def semantic_chunking(documents, language='english'):
     pass
 
-def sentence_chunking():
+def sentence_chunking(documents):
     pass
 
-def wordcount_chunking():
+def wordcount_chunking(documents, chunk_size) -> list:
     '''Chunks all documents by a specified and static word count. If document ends before chunk size is hit, it ends the chunk at end-of-document.'''
-    pass
+
+    chunked_documents = []
+
+    for doc in documents:
+        split_doc = doc.split()
+        doc_length = len(split_doc)
+
+        if doc_length > chunk_size:
+            beginning = 0
+
+            while beginning < doc_length:
+                end = min(beginning + chunk_size, doc_length)
+                current_chunk_array = split_doc[beginning:end]
+                chunked_documents.append(' '.join(current_chunk_array))
+                beginning = end
+
+        else:
+            chunked_documents.append(doc)
+
+    return chunked_documents
