e4244e783b41b29f2587243509b41c25ec46000c
Christopher Goodwin <54195497+cgoodwinDH@users.noreply.github.com>
2024-05-26 22:27:30 -0500

    created sentence chunking function

--- src/historianstexttools/chunk.py
+++ src/historianstexttools/chunk.py
@@ -4,12 +4,19 @@ def chunk(number_of_sentences=1, overlap=0, language='english'):
 def semantic_chunking(documents, language='english'):
     pass
 
-def sentence_chunking(documents):
-    pass
+def sentence_chunking(documents, language='english'):
+    from nltk.tokenize import sent_tokenize
 
-def wordcount_chunking(documents, chunk_size) -> list:
-    '''Chunks all documents by a specified and static word count. If document ends before chunk size is hit, it ends the chunk at end-of-document.'''
+    sentences = []
 
+    for doc in documents:
+        doc_sentences = sent_tokenize(doc, language=language)
+        for sentence in doc_sentences:
+            sentences.append(sentence)
+
+    return sentences
+
+def wordcount_chunking_without_overlap_and_sentence_breaking(documents, chunk_size):
     chunked_documents = []
 
     for doc in documents:
@@ -29,3 +36,15 @@ def wordcount_chunking(documents, chunk_size) -> list:
             chunked_documents.append(doc)
 
     return chunked_documents
+
+
+def wordcount_chunking(documents, chunk_size, overlap=0, break_at_sentence=False, language='english') -> list:
+    '''Chunks all documents by a specified and static word count. If document ends before chunk size is hit, it ends the chunk at end-of-document.
+    overlap: if less than 1 consider as a percentage, if greater than or equal to 1 consider as number of words'''
+
+    if overlap == 0 and break_at_sentence == False:
+        return wordcount_chunking_without_overlap_and_sentence_breaking(documents, chunk_size)
+
+    if break_at_sentence == True:
+        sentences = sentence_chunking(documents, language=language)
+
